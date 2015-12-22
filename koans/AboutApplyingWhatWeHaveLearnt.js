a5a9852f395b8fdaa3278fe59cba270466a36ff3
Mikael Wallen <mikael.wallen@autodesk.com>
2015-12-22 16:29:57 +0100

    Fix all final tests applying what we have learnt.

--- koans/AboutApplyingWhatWeHaveLearnt.js
+++ koans/AboutApplyingWhatWeHaveLearnt.js
@@ -32,7 +32,7 @@ describe("About Applying What We Have Learnt", function() {
         }
     }
 
-    expect(productsICanEat.length).toBe(FILL_ME_IN);
+    expect(productsICanEat.length).toBe(1);
   });
 
   it("given I'm allergic to nuts and hate mushrooms, it should find a pizza I can eat (functional)", function () {
@@ -41,7 +41,17 @@ describe("About Applying What We Have Learnt", function() {
 
       /* solve using filter() & all() / any() */
 
-      expect(productsICanEat.length).toBe(FILL_ME_IN);
+      var mushroomsTest = function(ingredient) {
+        return (ingredient === "mushrooms"); 
+      }
+
+      var pizzaFilter = function(pizza) {
+        return (pizza.containsNuts == false && _(pizza.ingredients).any(mushroomsTest) == false);
+      }
+
+      productsICanEat = _(products).filter(pizzaFilter);
+
+      expect(productsICanEat.length).toBe(1);
   });
 
   /*********************************************************************************/
@@ -55,14 +65,17 @@ describe("About Applying What We Have Learnt", function() {
       }
     }
 
-    expect(sum).toBe(FILL_ME_IN);
+    expect(sum).toBe(233168);
   });
 
   it("should add all the natural numbers below 1000 that are multiples of 3 or 5 (functional)", function () {
 
-    var sum = FILL_ME_IN;    /* try chaining range() and reduce() */
+    /* try chaining range() and reduce() */
+    var sum = _(_.range(1000)).chain().filter(function (x) {return (x % 3 === 0 || x % 5 === 0)})
+                                      .reduce(function (sum, x) { return sum + x })
+                                      .value();    
 
-    expect(233168).toBe(FILL_ME_IN);
+    expect(233168).toBe(sum);
   });
 
   /*********************************************************************************/
@@ -75,15 +88,29 @@ describe("About Applying What We Have Learnt", function() {
         }
     }
 
-    expect(ingredientCount['mushrooms']).toBe(FILL_ME_IN);
+    expect(ingredientCount['mushrooms']).toBe(2);
   });
 
   it("should count the ingredient occurrence (functional)", function () {
     var ingredientCount = { "{ingredient name}": 0 };
 
     /* chain() together map(), flatten() and reduce() */
+    // map out just ingredients arrays (in a new array)
+    // flatten the array to get one flat array of ingredients where the same ingredient might exist multiple times.
+    // reduce flat list to produce ingredientCount array
 
-    expect(ingredientCount['mushrooms']).toBe(FILL_ME_IN);
+    var reductionFunction = function (ingredients, ingredient) {
+      ingredients[ingredient] = (ingredient in ingredients) ? ingredients[ingredient] + 1 : 1 ;
+      return ingredients;
+    }
+
+    ingredientCount = 
+      _(products).chain().map(function (pizza) {return pizza.ingredients})
+                         .flatten()
+                         .reduce(reductionFunction, ingredientCount)
+                         .value();
+
+    expect(ingredientCount['mushrooms']).toBe(2);
   });
 
   /*********************************************************************************/
